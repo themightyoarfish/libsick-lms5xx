8077466ccb79fee945436225445cfa8073e79967
Rasmus Diederichsen <rasmus@psiori.com>
2022-04-02 21:21:45 +0200

    inscrutable scanf segmentation fault

--- sick.cpp
+++ sick.cpp
@@ -14,6 +14,9 @@
 using namespace std;
 using namespace Eigen;
 
+constexpr double DEG2RAD = 180.0 / M_PI;
+constexpr double RAD2DEG = 1 / DEG2RAD;
+
 enum class sick_err_t : uint8_t {
   Ok = 0,
   Sopas_Error_METHODIN_ACCESSDENIED,
@@ -84,7 +87,7 @@ using rad = double;
 using hz = double;
 
 struct LMSConfigParams {
-  hz freuency;
+  hz frequency;
   rad resolution;
   // from -95° to 95°
   rad start_angle;
@@ -101,6 +104,24 @@ static uint32_t ip_addr_to_int(const string &ip_str) {
   inet_pton(AF_INET, ip_str.c_str(), &ip_network);
 
   return ip_network;
+}
+
+static string ip_addr_to_hex_str(const string &ip_str) {
+  uint32_t ip_as_int = ntohl(ip_addr_to_int(ip_str));
+  char out[2 * 4 + 3 + 1]{0};
+  int sprintf_result =
+      sprintf(out, "%02X %02X %02X %02X",
+              // clang-format off
+                               (ip_as_int & 0xFF000000) >> 24,
+                               (ip_as_int & 0x00FF0000) >> 16,
+                               (ip_as_int & 0x0000FF00) >> 8,
+                               (ip_as_int & 0x000000FF) >> 0
+              // clang-format on
+      );
+  if (sprintf_result < 0) {
+    throw runtime_error("sprintf failed");
+  }
+  return out;
 }
 
 template <size_t NumPts = 1141> struct Scan {
@@ -145,6 +166,7 @@ public:
     addr.sin_port = htons(port);
     addr.sin_addr.s_addr = ip_addr_to_int(sensor_ip);
 
+    // TODO: connect timeout
     int connect_result = connect(
         sock_fd_, reinterpret_cast<struct sockaddr *>(&addr), sizeof(addr));
     if (connect_result < 0) {
@@ -208,10 +230,10 @@ enum SOPASCommand {
 /*         return 0 */
 
 static string method(const char *sopas_cmd, size_t len) {
-  if (len < sizeof("\x02 ...")) {
+  if (len < sizeof("\x02...")) {
     throw runtime_error("wat");
   } else
-    return string(sopas_cmd[2], sopas_cmd[5]);
+    return string(sopas_cmd+1, 3);
 }
 
 sick_err_t status_from_bytes_ascii(const char *data, size_t len) {
@@ -224,8 +246,8 @@ sick_err_t status_from_bytes_ascii(const char *data, size_t len) {
   const string answer_method = method(data, len);
   if (answer_method == "sFA") {
     // generic errors
-    static const string pattern = "\x02 sFA %2X \x03";
-    uint8_t status;
+    static const string pattern = "\x02sFA %u\x03";
+    unsigned int status = 0;
     int scanf_result = sscanf(data, pattern.c_str(), status);
     if (scanf_result != 1) {
       // parse error
@@ -235,14 +257,17 @@ sick_err_t status_from_bytes_ascii(const char *data, size_t len) {
     // proper response, but might have to get parsed specifically for
     // success/fail
     // skip STX and method, then identify response name, then check if there is
-    // a space after it, in which case any error code is "always" following. If
+    // a space after it, in which case any error code is always* following. If
     // 0, ok, otherwise not ok
+    // * retardedly, the Run command inverts this pattern. 1 = success, 0 =
+    // fail. i cant believe how stupid this is.
     return sick_err_t::Ok;
   }
 }
 
 static sick_err_t send_sopas_command(int sock_fd, const char *data,
                                      size_t len) {
+  std::cout << "Command: " << string(data, len) << std::endl;
   int send_result = send(sock_fd, data, len, 0);
   if (send_result < 0) {
     // error
@@ -253,7 +278,7 @@ static sick_err_t send_sopas_command(int sock_fd, const char *data,
   if (recv_result < 0) {
     // error
   }
-  std::cout << string(recvbuf.data()) << std::endl;
+  std::cout << "Command answer: " << string(recvbuf.data()) << std::endl;
   return status_from_bytes_ascii(recvbuf.data(), recv_result);
 }
 
@@ -262,7 +287,13 @@ class SOPASProtocolASCII : public SOPASProtocol {
   using SOPASProtocol::SOPASProtocol;
 
   map<SOPASCommand, string> command_masks_ = {
-      {SETACCESSMODE, "\x02sMN SetAccessMode %02d %08X\x03"}};
+      {SETACCESSMODE, "\x02sMN SetAccessMode %02d %08X\x03"},
+      {TSCROLE, "\x02sWN TSCRole %02d\x03"},
+      {TSCTCINTERFACE, "\x02sWN TSCTCInterface %02d\x03"},
+      {TSCTCSRVADDR, "\x02sWN TSCTCSrvAddr %s\x03"},
+      {MLMPSETSCANCFG, "\x02sMN mLMPsetscancfg +%4u +1 +%4u %d %d\x03"},
+      /* set_scancfg_cmd = "\x02sMN mLMPsetscancfg +2500 +1 +1667 -50000 +1850000\x03" */
+  };
 
 public:
   sick_err_t run() override { return sick_err_t::Ok; }
@@ -281,12 +312,60 @@ public:
     return result;
   }
 
+  template <typename... Args>
+  sick_err_t send_command(SOPASCommand cmd, Args... args) {
+    array<char, 4096> buffer;
+    // authorized client mode with pw hash from telegram listing
+    int bytes_written =
+        sprintf(buffer.data(), command_masks_[cmd].c_str(), args...);
+    if (bytes_written < 0) {
+      throw runtime_error("sprintf fail");
+    }
+
+    sick_err_t result =
+        send_sopas_command(sock_fd_, buffer.data(), bytes_written);
+    return result;
+  }
+
   sick_err_t configure_ntp_client(const string &ip) override {
-    return sick_err_t::Ok;
+    const sick_err_t role_res = send_command(TSCROLE, 1);
+    if (role_res != sick_err_t::Ok) {
+      return role_res;
+    }
+    const sick_err_t iface_res = send_command(TSCTCINTERFACE, 0);
+    if (iface_res != sick_err_t::Ok) {
+      return iface_res;
+    }
+    const sick_err_t srvaddr_res = send_command(
+        TSCTCSRVADDR,
+        ip_addr_to_hex_str(ip.c_str())
+            .c_str() /* convert to c str to pass to variadic sprintf */);
+    return srvaddr_res;
   }
 
   sick_err_t set_scan_config(const LMSConfigParams &params) override {
-    return sick_err_t::Ok;
+      // convert to degrees and add offset so 0 is straight ahead
+    auto angle_to_lms = [](rad angle_in) { return angle_in * RAD2DEG + 90; };
+
+    const hz frequency = params.frequency;
+    const unsigned int hz_Lms = static_cast<unsigned int>(frequency * 100);
+    const rad ang_increment = params.resolution;
+    const unsigned int ang_increment_lms =
+        static_cast<unsigned int>(round(ang_increment * 10000));
+    const int start_angle_lms =
+        static_cast<int>(angle_to_lms(params.start_angle) * 10000);
+    const int end_angle_lms =
+        static_cast<unsigned int>(angle_to_lms(params.end_angle) * 10000);
+
+    return send_command(MLMPSETSCANCFG, hz_Lms, ang_increment_lms,
+                        start_angle_lms, end_angle_lms);
+    /* set_scancfg_cmd = "\x02sMN mLMPsetscancfg +2500 +1 +1667 -50000 +1850000\x03" */
+    /*   hz freuency; */
+    /*   rad resolution; */
+    /*   // from -95° to 95° */
+    /*   rad start_angle; */
+    /*   rad end_angle; */
+
   }
 
   sick_err_t save_params() override { return sick_err_t::Ok; }
@@ -297,5 +376,7 @@ static void cbk(const Scan<1141> &scan) {}
 int main() {
   SOPASProtocolASCII proto("192.168.95.194", 2111, cbk);
   sick_err_t status = proto.set_access_mode();
+  status = proto.configure_ntp_client("192.168.95.44");
+  status = proto.set_scan_config(LMSConfigParams{.frequency = 25, .resolution = 0.1667, .start_angle = -95 * DEG2RAD, .end_angle = 95 * DEG2RAD});
   std::cout << sick_err_t_to_string(status) << std::endl;
 }
