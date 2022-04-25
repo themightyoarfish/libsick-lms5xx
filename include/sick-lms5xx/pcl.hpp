284d66ab79bf68372a8b217a202afe73dda2e973
Rasmus Diederichsen <rasmus@psiori.com>
2022-04-25 20:37:03 +0200

    Fix mismatch between build PCL compontents and install PCL components

--- include/sick-lms5xx/pcl.hpp
+++ include/sick-lms5xx/pcl.hpp
@@ -1,7 +1,11 @@
 #pragma once
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+
 namespace sick {
+
+class Scan;
+
 namespace pcl {
 
 ::pcl::PointCloud<::pcl::PointXYZI> cloud_from_scan(const sick::Scan &scan);
