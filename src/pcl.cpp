c1ebca719620033dc9de396c077538022b9fb50a
Rasmus Diederichsen <rasmus@psiori.com>
2022-04-27 18:37:09 +0200

    Make code possibly pcl 1.10 compatible

--- src/pcl.cpp
+++ src/pcl.cpp
@@ -5,13 +5,14 @@ namespace sick {
 namespace pcl {
 ::pcl::PointCloud<::pcl::PointXYZI> cloud_from_scan(const sick::Scan &scan) {
   ::pcl::PointCloud<::pcl::PointXYZI> cloud_out;
-  cloud_out.width = scan.ranges.size();
-  cloud_out.height = 1;
+  cloud_out.resize(scan.ranges.size());
   const Eigen::VectorXf x = scan.ranges.array() * scan.cos_map.array();
   const Eigen::VectorXf y = scan.ranges.array() * scan.sin_map.array();
   for (int i = 0; i < x.size(); ++i) {
-    cloud_out.points.emplace_back(
-        ::pcl::PointXYZI(x(i), y(i), 0, scan.intensities[i]));
+    cloud_out.points[i].x = x(i);
+    cloud_out.points[i].y = y(i);
+    cloud_out.points[i].z = 0;
+    cloud_out.points[i].intensity = scan.intensities[i];
   }
   return cloud_out;
 }
